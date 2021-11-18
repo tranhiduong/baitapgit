f5b34705f85cb89b2bf79791ac77072aa70088b1
Duong Tran <jeff@Duong-Computer.local>
2021-11-18 21:00:18 +0700

    test

--- source.cpp
+++ source.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
+
 int tong(int a, int b);
 int hieu(int a, int b)
 {
 	return a-b;
 };
-=======
 int tong(int a, int b)
 {
 	return a+b;
 };
 int hieu(int a, int b);
->>>>>>> 4263623bc9300d695eae97f8b8b9ecc9a0168a94
 
 int main()
 {
@@ -24,13 +22,11 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
-<<<<<<< HEAD
+
 	if(phepToan=='-')
         cout << "Ket qua hieu la =" << hieu(a,b)<<endl;   
-=======
 	if(phepToan=='+')
         cout << "Ket qua tong la =" << tong(a,b)<<endl;   
->>>>>>> 4263623bc9300d695eae97f8b8b9ecc9a0168a94
 	system("pause");
 	return 0;
 }
