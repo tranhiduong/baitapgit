4263623bc9300d695eae97f8b8b9ecc9a0168a94
Duong Tran <jeff@Duong-Computer.local>
2021-11-18 20:33:07 +0700

    phep tong

--- source.cpp
+++ source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b)
+{
+	return a+b;
+};
 int hieu(int a, int b);
 
 int main()
@@ -13,6 +16,8 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+	if(phepToan=='+')
+        cout << "Ket qua tong la =" << tong(a,b)<<endl;   
 	system("pause");
 	return 0;
 }
